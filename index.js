0c8dc4194daeed58b2080bb9976367675dfbc119
Shuvo Khan <shuvoprogramer@gmail.com>
2022-10-25 18:37:28 +0600

    added course data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(cors())
 
 const category = require('./data/CoursesCategory.json')
+const courses = require('./data/Courses.json')
 
 app.get('/', (req, res) => {
     res.send("Server Connected....")
@@ -13,6 +14,22 @@ app.get('/', (req, res) => {
 
 app.get('/category', (req, res) => {
     res.send(category);
+})
+
+app.get('/category/:Category_id', (req, res) => {
+    const category_id = req.params.Category_id;
+    const selectedCategory = category.find(n => n.Category_id == category_id)
+    res.send(selectedCategory);
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:Category_id', (req, res) => {
+    const course_id = req.params.Category_id;
+    const relatedCourse = courses.filter(n => n.Category_id == course_id)
+    res.send(relatedCourse)
 })
 
 app.listen(port, () => {
